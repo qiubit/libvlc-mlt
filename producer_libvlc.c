66e64d0ca2c1b587c251040abea94af7beba91cd
Pawel Golinski <golpaw1@gmail.com>
2016-06-09 17:48:28 +0200

    Setup smem during producer init

--- producer_libvlc.c
+++ producer_libvlc.c
@@ -13,6 +13,7 @@ struct producer_libvlc_s
 	mlt_producer parent;
 	libvlc_instance_t *libvlc_instance;
 	libvlc_media_t *media;
+	libvlc_media_player_t *media_player;
 	pthread_mutex_t video_mutex;
 	pthread_mutex_t audio_mutex;
 	uint8_t *video_buffer;
@@ -21,6 +22,7 @@ struct producer_libvlc_s
 // Forward references
 static int producer_get_frame( mlt_producer producer, mlt_frame_ptr frame, int index );
 static void collect_stream_data( producer_libvlc self, char *file );
+static void setup_smem( producer_libvlc self );
 static void audio_prerender_callback( void* p_audio_data, uint8_t** pp_pcm_buffer, size_t size );
 static void audio_postrender_callback( void* p_audio_data, uint8_t* p_pcm_buffer, unsigned int channels,
 									   unsigned int rate, unsigned int nb_samples, unsigned int bits_per_sample,
@@ -56,6 +58,11 @@ mlt_producer producer_libvlc_init( mlt_profile profile, mlt_service_type type, c
 
 				// Initialize media and metadata
 				collect_stream_data( self, file );
+
+				// Setup libVLC smem
+				setup_smem( self );
+				self->media_player = libvlc_media_player_new_from_media( self->media );
+				libvlc_media_player_play( self->media_player );
 
 				pthread_mutex_init( &self->video_mutex, NULL );
 				pthread_mutex_init( &self->audio_mutex, NULL );
@@ -133,15 +140,90 @@ static void collect_stream_data( producer_libvlc self, char *file )
 	libvlc_media_tracks_release( tracks, nb_tracks );
 }
 
+static void setup_smem( producer_libvlc self )
+{
+	int width, height, size, i;
+	float fps;
+	// Needed to make sure it is delimited by dot
+	char fps_string[ 1000 ];
+
+	char vcodec[] = "I420";
+	char acodec[] = "u8 ";
+
+	mlt_properties p = MLT_PRODUCER_PROPERTIES( self->parent );
+
+	// Collect metadata to local variables
+	width = mlt_properties_get_int( p, "meta.media.width" );
+	height = mlt_properties_get_int( p, "meta.media.height" );
+	fps = mlt_properties_get_int( p, "meta.media.frame_rate_num" )
+			/ mlt_properties_get_int( p, "meta.media.frame_rate_den" );
+
+	size = snprintf( fps_string, 1000, "%f", fps);
+	for (i = 0; i < size; i++)
+	{
+		if (fps_string[ i ] == ',')
+		{
+			fps_string[ i ] = '.';
+		}
+	}
+
+	// Build smem options string
+	char smem_options[ 1000 ];
+	sprintf( smem_options,
+			 ":sout=#transcode{"
+			 "vcodec=%s,"
+			 "fps=%s,"
+			 "width=%d,"
+			 "height=%d,"
+			 "acodec=%s,"
+			 "}:smem{"
+			 "no-time-sync,"
+			 "audio-prerender-callback=%lld,"
+			 "audio-postrender-callback=%lld,"
+			 "video-prerender-callback=%lld,"
+			 "video-postrender-callback=%lld,"
+			 "}",
+			 vcodec, fps_string, width, height, acodec,
+			 (long long int)(intptr_t)(void*)&audio_prerender_callback,
+			 (long long int)(intptr_t)(void*)&audio_postrender_callback,
+			 (long long int)(intptr_t)(void*)&video_prerender_callback,
+			 (long long int)(intptr_t)(void*)&video_postrender_callback );
+
+	// Supply smem options to libVLC
+	libvlc_media_add_option( self->media, smem_options );
+}
+
+static void audio_prerender_callback( void* p_audio_data, uint8_t** pp_pcm_buffer, size_t size )
+{
+	/* TODO */
+	printf("audio lock\n");
+	uint8_t *buffer = malloc( size * sizeof( uint8_t ) );
+	*pp_pcm_buffer = buffer;
+}
+
+static void audio_postrender_callback( void* p_audio_data, uint8_t* p_pcm_buffer, unsigned int channels,
+									   unsigned int rate, unsigned int nb_samples, unsigned int bits_per_sample,
+									   size_t size, int64_t pts )
+{
+	/* TODO */
+	printf("audio unlock\n");
+	free( p_pcm_buffer );
+}
+
 static void video_prerender_callback( void *data, uint8_t **p_buffer, size_t size )
 {
 	/* TODO */
+	printf("video lock\n");
+	uint8_t *buffer = malloc( size * sizeof( uint8_t ) );
+	*p_buffer = buffer;
 }
 
 static void video_postrender_callback( void *data, uint8_t *buffer, int width, int height,
 									   int bpp, size_t size, int64_t pts )
 {
 	/* TODO */
+	printf("video unlock\n");
+	free( buffer );
 }
 
 
