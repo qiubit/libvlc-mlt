3164af68e1d78bfa73935e9e5a4b60368d18d63e
Pawel Golinski <golpaw1@gmail.com>
2016-07-16 00:39:31 +0200

    consumer_libvlc: REALLY register metadata in MLT

--- factory.c
+++ factory.c
@@ -1,12 +1,22 @@
 #include <string.h>
 #include <limits.h>
+
 #include <framework/mlt.h>
 
 extern mlt_producer producer_libvlc_init( mlt_profile profile, mlt_service_type type, const char *id, char *arg );
 extern mlt_consumer consumer_libvlc_init( mlt_profile profile, mlt_service_type type, const char *id, char *arg );
 
+static mlt_properties metadata( mlt_service_type type, const char *id, void *data )
+{
+	char file[ PATH_MAX ];
+	snprintf( file, PATH_MAX, "%s/libvlc/%s", mlt_environment( "MLT_DATA" ), (char*) data );
+	return mlt_properties_parse_yaml( file );
+}
+
 MLT_REPOSITORY
 {
 	MLT_REGISTER( producer_type, "libvlc", producer_libvlc_init );
 	MLT_REGISTER( consumer_type, "libvlc", consumer_libvlc_init );
+
+	MLT_REGISTER_METADATA( consumer_type, "libvlc", metadata, "consumer_libvlc.yml" );
 }
